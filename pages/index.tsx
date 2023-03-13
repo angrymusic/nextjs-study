f62bf97ab9dbe1ffbf7eac9fc30e5aeb771d71de
angrymusic <angrymusic@naver.com>
2023-03-13 17:40:33 +0900

    영화 데이터 fetch 후 출력

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
 import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "19a25cabd9d79c306165678f6a258be2";
+
+interface Movie {
+    id: number;
+    original_title: string;
+}
 export default function Home() {
+    const [movies, setMovies] = useState<Movie[]>();
+    useEffect(() => {
+        (async () => {
+            const { results } = await (
+                await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+            ).json();
+            setMovies(results);
+        })();
+    }, []);
     return (
         <>
             <Seo title="Home"></Seo>
-            <h1>HOME</h1>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div key={movie.id}>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
         </>
     );
 }
